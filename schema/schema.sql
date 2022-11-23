dca9a84c53b631af54311b20ba369b6c33a7e74b
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-11-23 16:29:29 +0900

    딜레이 수정

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,2 +1,4 @@
-INSERT INTO Managers (level, token)
-VALUES (5, 'fc9bee1f-41b1-46cc-843c-f6419f831004');
+-- INSERT INTO Products (type, name, price) VALUES (0, '입장 기념 지급', 500);
+-- INSERT INTO Products (type, name, price) VALUES (1, '음료수 1컵', 200);
+
+delete from Users where username = 'ㅅㄷㄴㅅ';
